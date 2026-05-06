c08ec157a37e2a4acd6015f1b00a2f2215853496
Khai Mohammad <khmr2024@mymail.pomona.edu>
2026-05-06 15:17:30 -0700

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.py
@@ -0,0 +1,43 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        # we need to look for cycles in a graph
+        # edge cases
+        # so, first off, I'm thinking about a graph and and adj list
+        # graphs usually involve some type of search, BFS, DFS, Disjoint Set Union, etc
+        # How do we actually find cycles in a graph?
+        # Answer should be obvious, we have a destination already in our visited set
+        # We need to keep track of visited nodes within the graph
+        # Once we find a node thats been tracked, we need to have its edge be the current "latest" edge we've tracked
+        # We can also use union find for this! by keeping track of parents we instantly find a cycle
+        N = len(edges)
+        rank = [1] * (N+1)
+        par = [i for i in range(N+1)]
+
+        def find(n):
+            p = par[n]
+
+            while p != par[p]:
+                par[p] = par[par[p]]
+                p = par[p]
+            return p
+
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return False
+            if rank[p1] > rank[p2]:
+                par[p2] = p1
+                rank[p1] +=rank[p2]
+            else:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            return True
+        
+        for n1, n2 in edges:
+            if not union(n1, n2):
+                return [n1, n2]
+
+
+
+        
