efa49e25a0b091a650c6ce5066753c4ef7c1ab0e
Khai Mohammad <khmr2024@mymail.pomona.edu>
2026-05-10 07:52:04 -0700

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0: return 0
+    
+        queue = deque([(amount, 0)])
+        visited = {amount} 
+        
+        while queue:
+            curr_amt, count = queue.popleft()
+            
+            for coin in coins:
+                next_amt = curr_amt - coin
+                
+                if next_amt == 0:
+                    return count + 1 
+                    
+                if next_amt > 0 and next_amt not in visited:
+                    visited.add(next_amt)
+                    queue.append((next_amt, count + 1))
+                    
+        return -1
+
+       
