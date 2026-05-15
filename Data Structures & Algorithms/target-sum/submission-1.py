c4b029c446c7499facf4d4a5eab4ff84c819dd98
Khai Mohammad <khmr2024@mymail.pomona.edu>
2026-05-15 12:06:03 -0700

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        # Brute force solution would be a dfs algorithim that has 2 choices at each turn
+        # This would result in O(2^n) time complexity and due to recursion stacks O(n) memory
+        # However, we can optimize this problem
+        # The question here is if we had a list of 4 numbers
+        # we add the first two which would result in 3 different outcomes 
+        # The question is we can actually do backwards with a dynampic programming approach
+        # We can pass down the 
+        def dfs(total, i):
+            if i == len(nums):
+                return 1 if total == target else 0
+            
+            return dfs(total-nums[i], i+1) + dfs(total+nums[i], i+1)
+        
+        return dfs(0, 0)
